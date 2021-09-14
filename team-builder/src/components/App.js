c500f0a80b5ead14c61822e505d256c460a5c444
IrvinA <arevalosirvin@gmail.com>
2021-09-14 12:06:49 -0700

    App.js is all setup and done

--- team-builder/src/components/App.js
+++ team-builder/src/components/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const membersList = [];
+
+const initialFormValues = { name: "", age:"", email:"", role:"" }
 
 function App() {
+  const [members, setMembers] = useState(membersList);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [error, setError] = useState("");
+
+  const updateForm = (input, value) => {
+    setFormValues({ ...formValues, [input]: value});
+  }
+
+  const submitForm = () => {
+    const newMember = {
+      name: formValues.name.trim(),
+      age: formValues.age.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    }
+
+    if (!newMember.name) {
+      setError("Please type in your Name");
+    } else if (!newMember.age) {
+      setError("Please type in your Age");
+    } else if (!newMember.email) {
+      setError("Please type in a valid Email");
+    } else if (!newMember.role) {
+      setError("Please choose your Role");
+    } else {
+      setError("");
+    }
+
+    setMembers([newMember, ...members]);
+    setFormValues(initialFormValues);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>New Members Sign Up!</h1>
+
+      {error &&<h2 className='error-text'>{error}</h2>}
+      <TeamForm
+        update={updateForm}
+        submit={submitForm}
+        values={formValues}
+      />
+
+      {
+        members.map(member => {
+          return (
+            <TeamMember key={member.id} details={member} />
+          )
+        })
+      }
     </div>
   );
 }
